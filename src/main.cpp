b40d1e5b1b3df3d94446905255b5c3a908bbc5c7
yagiz erdem <yagizerdem819@gmail.com>
2026-07-24 12:24:22 +0300

    refactor: move traverse function to util module and reorganize source files

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,13 @@
 #include <string>
 #include <cpp-tree-sitter.h>
 #include <stdio.h>
-#include <cstdio>
-#include <iostream>
 #include <tree_sitter/api.h>
+#include "util.h"
 
 extern "C" {
 TSLanguage* tree_sitter_bash(void);
 }
 
-void traverse(TSNode root, int depth = 0) {
-    std::cout
-        << std::string(depth * 2, ' ')
-        << ts_node_type(root)
-        << '\n';
-
-    TSTreeCursor cursor = ts_tree_cursor_new(root);
-
-    if (ts_tree_cursor_goto_first_child(&cursor)) {
-        do {
-            TSNode child =
-                ts_tree_cursor_current_node(&cursor);
-            if (ts_node_start_byte())
-
-            traverse(child, depth + 1);
-
-        } while (ts_tree_cursor_goto_next_sibling(&cursor));
-    }
-
-    ts_tree_cursor_delete(&cursor);
-}
 
 int main() {
 
---
+++ src/util.h
@@ -0,0 +1,10 @@
+//
+// Created by yagiz on 24.07.2026.
+//
+
+#ifndef CLASH_UTIL_H
+#define CLASH_UTIL_H
+
+void traverse(TSNode root, int depth = 0);
+
+#endif //CLASH_UTIL_H

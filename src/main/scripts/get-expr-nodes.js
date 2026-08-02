eddb2935306deab57824967314ebfa67f93df907
yagiz erdem <yagizerdem819@gmail.com>
2026-08-02 20:51:26 +0300

    feat: add expression and primary expression node type definitions with extraction scripts

---
+++ src/main/scripts/get-expr-nodes.js
@@ -0,0 +1,46 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const data = readFileSync("./node-types.json", { encoding: "utf8" });
+const jsonData = JSON.parse(data);
+
+const set = new Set();
+const stack = ["_expression"];
+
+while (stack.length > 0) {
+  const current = stack.pop();
+  set.add(current);
+
+  const node = jsonData.find((node) => node.type === current);
+
+  if (!node?.subtypes) {
+    continue;
+  }
+
+  for (const subtype of node.subtypes) {
+    if (subtype.type == "_primary_expression") {
+      continue;
+    }
+
+    if (!set.has(subtype.type)) {
+      stack.push(subtype.type);
+    }
+  }
+}
+
+const exprNodes = [];
+
+jsonData.forEach((node) => {
+  if (set.has(node.type)) {
+    exprNodes.push(node);
+  }
+});
+
+console.dir(exprNodes, {
+  maxArrayLength: null,
+});
+
+console.log(`Found ${exprNodes.length} expression nodes.`);
+
+writeFileSync("./expr-nodes.json", JSON.stringify(exprNodes, null, 2), {
+  encoding: "utf8",
+});
---
+++ src/main/scripts/get-primary-expr-nodes.js
@@ -0,0 +1,46 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const data = readFileSync("./node-types.json", { encoding: "utf8" });
+const jsonData = JSON.parse(data);
+
+const set = new Set();
+const stack = ["_primary_expression"];
+
+while (stack.length > 0) {
+  const current = stack.pop();
+  set.add(current);
+
+  const node = jsonData.find((node) => node.type === current);
+
+  if (!node?.subtypes) {
+    continue;
+  }
+
+  for (const subtype of node.subtypes) {
+    if (!set.has(subtype.type)) {
+      stack.push(subtype.type);
+    }
+  }
+}
+
+const primaryExprNodes = [];
+
+jsonData.forEach((node) => {
+  if (set.has(node.type)) {
+    primaryExprNodes.push(node);
+  }
+});
+
+console.dir(primaryExprNodes, {
+  maxArrayLength: null,
+});
+
+console.log(`Found ${primaryExprNodes.length} primary expression nodes.`);
+
+writeFileSync(
+  "./primary-expr-nodes.json",
+  JSON.stringify(primaryExprNodes, null, 2),
+  {
+    encoding: "utf8",
+  },
+);
